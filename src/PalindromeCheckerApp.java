621135d64a08e345f517884e36eeb11c903356f8
anvita104 <anvitaanil55@gmail.com>
2026-02-27 15:28:47 +0530

    UC3:Palindromecheckusingstringreverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,22 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String original = "level";
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println("Original String:" + original);
+            System.out.println("Reversed String" + reversed);
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("Original String:" + original);
+            System.out.println("Reversed String" + reversed);
+            System.out.println("The given string is NOT a Palindrome.");
 
-        public static void main(String[] args) {
-            String word = "madam";
-            String reversed = "";
-            for (int i = word.length() - 1; i >= 0; i--) {
-                reversed = reversed + word.charAt(i);
-            }
-            if (word.equals(reversed)) {
-                System.out.println(word + " is a Palindrome.");
-            } else {
-                System.out.println(word + " is NOT a Palindrome.");
-            }
+
         }
     }
+}
 
 
