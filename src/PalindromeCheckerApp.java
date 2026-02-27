f7b125803f3a9345eaa61179bec9ef723b4f24d8
anvita104 <anvitaanil55@gmail.com>
2026-02-27 15:53:22 +0530

    UC4:CharacterArrayBasedPalindromeCheck

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,25 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String original = "level";
-        String reversed = "";
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+    public static void main(String[] args ){
+        String input = "radar";
+        char[] charArray = input.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if (original.equals(reversed)) {
-            System.out.println("Original String:" + original);
-            System.out.println("Reversed String" + reversed);
+        System.out.println("Input string :" + input);
+        if (isPalindrome) {
             System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println("Original String:" + original);
-            System.out.println("Reversed String" + reversed);
             System.out.println("The given string is NOT a Palindrome.");
-
-
         }
     }
 }
