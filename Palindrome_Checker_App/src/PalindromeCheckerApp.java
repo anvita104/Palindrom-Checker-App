171bf40f84be2a715ee24b8fe5663039b590c276
anvita104 <anvitaanil55@gmail.com>
2026-03-09 15:44:39 +0530

    UC9:Recursive Palindrome Checker

--- Palindrome_Checker_App/src/PalindromeCheckerApp.java
+++ Palindrome_Checker_App/src/PalindromeCheckerApp.java
@@ -1,73 +1,35 @@
-class Node {
-    char data;
-    Node next;
-    Node(char data) { this.data = data; this.next = null; }
-}
-
-class LinkedList {
-    Node head;
-
-    void addString(String str) {
-        for (char ch : str.toCharArray()) {
-            Node newNode = new Node(ch);
-            if (head == null) head = newNode;
-            else {
-                Node temp = head;
-                while (temp.next != null) temp = temp.next;
-                temp.next = newNode;
-            }
-        }
-    }
-
-    Node reverse(Node node) {
-        Node prev = null, curr = node, next = null;
-        while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
-    boolean isPalindrome() {
-        if (head == null || head.next == null) return true;
-
-        Node slow = head, fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow.next);
-        Node firstHalf = head, tempSecond = secondHalf;
-        boolean palindrome = true;
-
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                palindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
-        }
-
-        slow.next = reverse(secondHalf);
-        return palindrome;
-    }
-}
-
+import java.util.Scanner;
 public class PalindromeCheckerApp {
+    // Recursive method to check palindrome
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters do not match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
-        String input = "racecar"; // hardcoded input
-        System.out.println("Checking string: " + input);
 
-        LinkedList list = new LinkedList();
-        list.addString(input.replaceAll("\\s+", "").toLowerCase());
+        Scanner scanner = new Scanner(System.in);
 
-        if (list.isPalindrome())
-            System.out.println("The string is a palindrome!");
-        else
-            System.out.println("The string is NOT a palindrome.");
+        System.out.println("Palindrome Checker App - Recursive Method");
+        System.out.print("Enter a string: ");
+
+        String input = scanner.nextLine();
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+        if (result) {
+            System.out.println("The given string is a palindrome.");
+        } else {
+            System.out.println("The given string is NOT a palindrome.");
+        }
+
+        scanner.close();
     }
 }
