2d4902a9446f4b74077feb61bd0672786455177c
anvita104 <anvitaanil55@gmail.com>
2026-03-16 13:44:31 +0530

    UC13:performance comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,85 @@
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String input = "madam";
-        Deque<Character> deque = new LinkedList<>();
-        for (int i = 0; i < input.length(); i++) {
-            deque.addLast(input.charAt(i));
+
+    // Reverse String Method
+    public static boolean reverseMethod(String str) {
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
         }
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            char frontChar = deque.removeFirst();
-            char rearChar = deque.removeLast();
-            if (frontChar != rearChar) {
-                isPalindrome = false;
-                break;
+
+        return str.equals(reversed);
+    }
+
+    // Stack Method
+    public static boolean stackMethod(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
             }
         }
-        if (isPalindrome) {
-            System.out.println(input + " is a Palindrome.");
-        } else {
-            System.out.println(input + " is NOT a Palindrome.");
+
+        return true;
+    }
+
+    // Two Pointer Method
+    public static boolean twoPointerMethod(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Palindrome Checker App - Performance Comparison");
+        System.out.print("Enter a string: ");
+
+        String input = scanner.nextLine();
+
+        // Reverse Method Timing
+        long startTime = System.nanoTime();
+        boolean reverseResult = reverseMethod(input);
+        long endTime = System.nanoTime();
+        long reverseTime = endTime - startTime;
+
+        // Stack Method Timing
+        startTime = System.nanoTime();
+        boolean stackResult = stackMethod(input);
+        endTime = System.nanoTime();
+        long stackTime = endTime - startTime;
+
+        // Two Pointer Method Timing
+        startTime = System.nanoTime();
+        boolean pointerResult = twoPointerMethod(input);
+        endTime = System.nanoTime();
+        long pointerTime = endTime - startTime;
+
+        System.out.println("\nResults:");
+        System.out.println("Reverse Method Result: " + reverseResult + " | Time: " + reverseTime + " ns");
+        System.out.println("Stack Method Result: " + stackResult + " | Time: " + stackTime + " ns");
+        System.out.println("Two Pointer Method Result: " + pointerResult + " | Time: " + pointerTime + " ns");
+
+        scanner.close();
     }
 }
